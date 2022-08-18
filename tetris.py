e254d5f1fdab600dedd74fbd5ddd88f3c566292e
YatoNoragami1 <maximus17345@gmail.com>
2022-08-18 15:57:36 +0200

    collision bitch

--- tetris.py
+++ tetris.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
@@ -9,6 +10,7 @@ window_y = 600
 clock = pygame.time.Clock()
 fps = 15
 current_turn = 0
+current_piece = 1
 
 pygame.init()
 pygame.joystick.init()
@@ -29,7 +31,10 @@ T_BLOCK = [[window_x // 2, window_y // 2], [(window_x // 2)+30, window_y // 2], 
 
 Z_BLOCK = [[window_x // 2, window_y // 2],[(window_x // 2)+30, window_y // 2],[window_x // 2, (window_y // 2)-30],[(window_x // 2)-30, (window_y // 2)-30]]
 
+BOARD = []
 
+current_state = I_BLOCK
+selected_piece = I_BLOCK
 direction = ' '
 running = True
 
@@ -41,6 +46,9 @@ def draw_T_Block(current_state):
     for i in current_state:
         pygame.draw.rect(game_window, BLUE, (i[0], i[1], 30, 30)) 
 def draw_J_Block(current_state):
+    for i in current_state:
+        pygame.draw.rect(game_window, BLUE, (i[0], i[1], 30, 30)) 
+def draw_L_Block(current_state):
     for i in current_state:
         pygame.draw.rect(game_window, BLUE, (i[0], i[1], 30, 30)) 
 def draw_O_Block(current_state):
@@ -56,9 +64,9 @@ def draw_Z_Block(current_state):
 def Turn():
     global direction
     global current_turn
-    print(current_turn)
-    current_piece = 2
-    current_state = I_BLOCK
+    global current_piece
+    global selected_piece
+    global current_state
 #I BLOCK
     if current_piece == 0:
         if direction == 'UP':
@@ -74,6 +82,7 @@ def Turn():
             if current_turn == 2:
                 current_turn = 0
                 Turn()
+        selected_piece = I_BLOCK
 # T BLOCK
     if current_piece == 1:
         if direction == 'UP':
@@ -86,15 +95,19 @@ def Turn():
             if current_turn == 1:
                 current_state = [[window_x//2, window_y//2], [window_x//2, (window_y//2)-30], [window_x//2, (window_y//2)+30], [(window_x//2)+30, window_y//2]]
                 draw_T_Block(current_state)
+                move_piece()
             if current_turn == 2:
                 current_state = [[window_x // 2, window_y // 2], [(window_x // 2)+30, window_y // 2], [(window_x // 2)-30, window_y // 2], [window_x // 2, (window_y // 2)+30]]
                 draw_T_Block(current_state)
+                move_piece
             if current_turn == 3:
                 current_state = [[window_x//2, window_y//2], [window_x//2, (window_y//2)-30], [window_x//2, (window_y//2)+30], [(window_x//2)-30, window_y//2]]
                 draw_T_Block(current_state)
+                move_piece
             if current_turn == 4:
                 current_turn = 0
                 Turn()
+        selected_piece = T_BLOCK
 #J BLOCK
     if current_piece == 2:
         if direction == 'UP':
@@ -105,10 +118,10 @@ def Turn():
                 current_state = J_BLOCK
                 draw_J_Block(current_state)
             if current_turn == 1:
-                current_state = [[window_x //2, window_y //2], [(window_x // 2)-30, window_y // 2], [(window_x //2)+30, window_y // 2], [(window_x // 2)-30, (window_y // 2)-30]]
+                current_state = [[window_x //2, window_y //2], [(window_x // 2)-30, window_y // 2], [(window_x //2)+30, window_y // 2], [(window_x // 2)+30, (window_y // 2)+30]]
                 draw_J_Block(current_state)
             if current_turn == 2:
-                current_state = [[window_x //2, window_y //2], [(window_x // 2)-30, window_y // 2], [(window_x //2)+30, window_y // 2], [(window_x // 2)+30, (window_y // 2)-30]]
+                current_state = [[window_x //2, window_y //2], [(window_x // 2)+30, window_y // 2], [(window_x //2), (window_y // 2)+30], [window_x // 2, (window_y // 2)+60]]
                 draw_J_Block(current_state)
             if current_turn == 3:
                 current_state = [[window_x //2, window_y //2], [(window_x // 2)-30, window_y // 2], [(window_x //2)+30, window_y // 2], [(window_x // 2)-30, (window_y // 2)-30]]
@@ -116,6 +129,7 @@ def Turn():
             if current_turn == 4:
                 current_turn = 0
                 Turn()
+        selected_piece = J_BLOCK
 #S BLOCK
     if current_piece ==3:
         if direction == 'UP':
@@ -123,20 +137,15 @@ def Turn():
             direction = ' '
         if direction == ' ':
             if current_turn == 0:
-                current_state = J_BLOCK
-                draw_J_Block(current_state)
+                current_state = S_BLOCK
+                draw_S_Block(current_state)
             if current_turn == 1:
-                current_state = 
-                draw_J_Block(current_state)
+                current_state = [[window_x // 2, window_y // 2],[(window_x // 2)+30, window_y // 2],[window_x // 2, (window_y // 2)-30],[(window_x // 2)+30, (window_y // 2)+30]]
+                draw_S_Block(current_state)
             if current_turn == 2:
-                current_state =               
-                draw_J_Block(current_state)
-            if current_turn == 3:
-                current_state =
-                draw_J_Block(current_state)
-            if current_turn == 4:
                 current_turn = 0
                 Turn()
+        selected_piece = S_BLOCK
 #Z BLOCK
     if current_piece == 4:
         if direction == 'UP':
@@ -144,24 +153,50 @@ def Turn():
             direction = ' '
         if direction == ' ':
             if current_turn == 0:
-                current_state = J_BLOCK
-                draw_J_Block(current_state)
+                current_state = Z_BLOCK
+                draw_Z_Block(current_state)
             if current_turn == 1:
-                current_state = 
-                draw_J_Block(current_state)
+                current_state = [[window_x // 2, window_y // 2],[(window_x // 2)-30, window_y // 2],[window_x // 2, (window_y // 2)+30],[(window_x // 2)-30, (window_y // 2)-30]]
+                draw_Z_Block(current_state)
             if current_turn == 2:
-                current_state =               
-                draw_J_Block(current_state)
-            if current_turn == 3:
-                current_state =
-                draw_J_Block(current_state)
-            if current_turn == 4:
                 current_turn = 0
                 Turn()
+        selected_piece = Z_BLOCK
+#L BLOCK
+    if current_piece == 5:
+        if direction == 'UP':
+            current_turn += 1
+            direction = ' '
+        if direction == ' ':
+            if current_turn == 0:
+                current_state = L_BLOCK
+                draw_L_Block(current_state)
+            if current_turn == 1:
+                current_state = [[window_x //2, window_y //2], [(window_x // 2)+30, window_y // 2], [(window_x //2)-30, window_y // 2], [(window_x // 2)-30, (window_y // 2)+30]]
+                draw_L_Block(current_state)
+            if current_turn == 2:
+                current_state = [[window_x //2, window_y //2], [(window_x // 2)-30, window_y // 2], [(window_x //2), (window_y // 2)+30], [window_x // 2, (window_y // 2)+60]]
+                draw_L_Block(current_state) 
+            if current_turn == 3:
+                current_turn = 0
+                Turn()
+        selected_piece = L_BLOCK
+				
+def choose_piece():
+	global current_piece
+	new_piece = random.randint(0, 6)
+	current_piece = new_piece
 
+def move_piece():
+	global current_state
+	BOARD = []
+	for i in current_state:
+		if i[1] <= window_y-60:
+			i[1] += 30
+		else:
+			break
 
-
-
+#choose_piece()
 while running:
     clock.tick(fps)
     for event in pygame.event.get():
@@ -182,4 +217,5 @@ while running:
                     direction = ' '
     game_window.fill(BLACK)
     Turn()
+    move_piece()
     pygame.display.update()
